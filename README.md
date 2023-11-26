3f75cdb891af2d1cf3ad337965a74fed9806d178
shadyaabed <shady@moltaqa.net>
2023-11-26 09:50:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# AqarPlus_website
---
+++ locales/en-US.js
@@ -0,0 +1,65 @@
+export default {
+  lang: 'ltr',
+
+  nav: {
+    home: 'HOME',
+    about: 'ABOUT APP',
+    screen: 'APP SCREENS',
+    download: 'DOWNLOAD NOW',
+    contact: 'CONTACT US',
+    terms: 'TERMS & CONDITIONS',
+    policy: 'Privacy Policy',
+    backToHome: 'BACK TO HOME',
+    lang: 'ع',
+  },
+  registerLink: 'REGISTER AS DRIVER',
+  mainSlider: [
+    {
+      mainTitle: 'About Aqar Plus',
+      title: 'An application specialized in displaying and providing real estate to users',
+      desc: 'The "Aqar Plus" team consists of a group of professionals in the field of real estate and information technology. We work passionately to provide an easy and distinctive experience for users through our digital platform. We aim to provide an efficient and reliable way for buyers, renters and owners to share and explore properties with ease and convenience.',
+    },
+  ],
+  feature: [
+    {
+      image: '1.svg',
+      title: "A wide variety of properties",
+      desc: 'Aqar Plus provides a variety of properties that include apartments, villas, lands, commercial offices, and more. Users can browse available properties and find those that fit their needs and budget.',
+    },
+    {
+      image: '2.svg',
+      title: "Simple and easy to use user interface",
+      desc: "The application is characterized by its simple and comfortable design that makes it easy for users to use and browse properties easily. It allows users to customize search criteria and filter results to obtain accurate and detailed information about available properties.",
+    },
+    {
+      image: '3.svg',
+      title: "Comprehensive and accurate information",
+      desc: 'The application provides comprehensive and detailed information about each property including detailed description, photos, location, property features, and contact information for direct communication with the seller or owner.'
+    },
+  ],
+  about: {
+    mainTitle: 'Aqar Plus app is your ideal choice',
+    title: 'About Aqar Plus',
+    desc: "<p>A multi-service online store application that allows users to purchase products and services online and have them delivered to their address. The application also includes reservation options for restaurants and events, and facilitates the process of obtaining the required services in an easy and convenient way. Those who are looking for a modern and sophisticated shopping experience, as it provides many distinctive and convenient services that make online shopping easy and enjoyable. Therefore, the application can be downloaded easily and benefit from all the services available on it with comfort and confidence. Get the best online shopping experience with the Pring application. Enjoy competitive prices and high quality of the products you buy through the Aqar Plus app. Fast and reliable delivery for all your purchases, with secure and easy payment options</p>",
+  },
+  downloadText: "<p>You can download the Aqar Plus app now by visiting the App Store on your smartphone, searching for Aqar Plus, and then downloading the app on your smartphone.</p>",
+  isoLink: "",
+  androidLink: "",
+  address: 'KSA - Jeddah',
+  terms: {
+    title: 'Terms And Conditions',
+  },
+  policy: {
+    title: 'Privacy Policy',
+  },
+  copyWrite: '©2023 All Rights Reserved',
+
+  contact: {
+    title: 'Contact Us',
+    name: 'name',
+    email: 'e-mail',
+    phone: 'phone',
+    message: 'message',
+    send: 'send ',
+    },
+}
